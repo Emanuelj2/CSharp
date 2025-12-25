e0e2bf42f8a26a1092c4511bf0942379ad5d2467
Emanuel Jose <emanueljose0522@hotmail.com>
2025-12-25 00:00:07 -0500

    finished the login process

--- UsersApp/UsersApp/Controllers/HomeController.cs
+++ UsersApp/UsersApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UsersApp.Models;
 
@@ -11,6 +12,7 @@ namespace UsersApp.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
