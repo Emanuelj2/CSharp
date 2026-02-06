ab07dd30e81786d061e8cb2c788702006c4788ac
Emanuel Jose <emanueljose0522@hotmail.com>
2026-02-06 10:19:02 -0500

    this is a simple stock market practice code

---
+++ OOP/StockMarket/StockMarket/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StockMarket.Code;
+
+public class Prorgam
+{
+
+    static void Main(string[] args)
+    {
+        StockManager market = new StockManager();
+
+        market.AddStock(new Stock("Microsoft", 200));
+        market.AddStock(new Stock("Google", 330));
+        market.AddStock(new Stock("Apple", 200));
+
+
+        // Try valid update
+        market.UpdateStock("Microsoft", 230.0);
+
+        // Try invalid big jump
+        market.UpdateStock("Microsoft", 400.0);
+
+        // Try negative
+        market.UpdateStock("Apple", -50);
+
+        // Try missing stock
+        market.UpdateStock("Google", 100);
+
+        //is not in the market
+        market.UpdateStock("Nvidia", 200);
+
+        Console.WriteLine("\nDone. Press any key...");
+        Console.ReadKey();
+
+
+    }
+}
