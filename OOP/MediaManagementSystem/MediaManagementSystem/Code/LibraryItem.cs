7e88b9b4718d8cd8a292d1d6319a9b605b504481
Emanuel Jose <emanueljose0522@hotmail.com>
2026-02-06 19:47:32 -0500

    made a library manager

---
+++ OOP/MediaManagementSystem/MediaManagementSystem/Code/LibraryItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaManagementSystem.Code
+{
+    internal class LibraryItem
+    {
+        public string Title { get; protected set; }
+        public int UniqueId { get; protected set; }
+        public bool Borrowed { get; protected set; }
+
+
+        public LibraryItem( int id, string title)
+        { 
+            Title = title;
+            UniqueId = id;
+            Borrowed = false;
+        }
+
+
+        public bool isBorrowed()
+        {
+            return Borrowed; 
+        }
+
+        public virtual void Borrow()
+        {
+            if(Borrowed)
+            {
+                Console.WriteLine("Item is borrowed");
+                return;
+            }
+
+            Borrowed = true;
+            Console.WriteLine($"{Title} has been borrowed");
+        }
+
+        public virtual void Return()
+        {
+            if(!Borrowed)
+            {
+                Console.WriteLine("Item is not currently borrowed");
+                return;
+            }
+
+            Borrowed = false;
+            Console.WriteLine($"{Title} has been returned.");
+        }
+
+        public virtual string GetInfo()
+        {
+            return $"ID: {UniqueId}, Title: {Title}, Borrowed: {Borrowed}";
+        }
+
+        //public abstract double GetLateFee(int daysLate);
+    }
+}
---
+++ OOP/MediaManagementSystem/MediaManagementSystem/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using MediaManagementSystem.Code;
+
+public class Program
+{
+    static void Main()
+    {
+        LibraryManager manager = new LibraryManager();
+
+        // Create items
+        Book book1 = new Book(1, "1984", "George Orwell", 328);
+        DvD dvd1 = new DvD(2, "Inception", 148, 13);
+
+
+        // Add items
+        manager.AddItem(book1);
+        manager.AddItem(dvd1);
+
+        // Test borrowing
+        manager.BorrowItem(1);
+        manager.BorrowItem(2);
+
+        // Print info
+        manager.PrintAllItems();
+
+        // Return item
+        manager.ReturnItem(1);
+
+        // Print again
+        manager.PrintAllItems();
+
+        
+    }
+}
