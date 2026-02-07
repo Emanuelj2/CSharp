4ee82ca1ff0ae9310888fa96fd20c839f94b813f
emanul <emnaueljose0522@gmail.com>
2026-02-06 19:24:23 -0500

    linked list

---
+++ SingleLinkedList/SingleLinkedList/Program.cs
@@ -0,0 +1,63 @@
+﻿// make the node class
+public class Node<T>
+{
+    public T Data;
+    public Node<T> Next;
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+//single linked list class
+public class SingleLinkedList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    //make constructor
+    public SingleLinkedList()
+    {
+        head = null;
+        tail = null;
+    }
+
+    public void AddNode(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+
+        newNode.Next = head;
+        head = newNode;
+        if (tail == null)
+        {
+            tail = newNode;
+        }
+    }
+
+
+    public void PintList()
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            Console.Write(current.Data + " -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SingleLinkedList<int> list = new SingleLinkedList<int>();
+        list.AddNode(10);
+        list.AddNode(20);
+        // Add nodes and test the list here
+        list.PintList();
+    }
+}
