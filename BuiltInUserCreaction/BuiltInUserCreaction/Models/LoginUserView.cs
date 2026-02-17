d2e6176a8b69e8419c85483bbfc5e3a376cbe12d
Emanuel Jose <emanueljose0522@hotmail.com>
2026-02-17 11:41:38 -0500

    Using User Identity

---
+++ BuiltInUserCreaction/BuiltInUserCreaction/Models/LoginUserView.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuiltInUserCreaction.Models
+{
+    public class LoginUserView
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
