ccf83e33f9cc65e9e59b1fd1dab339c06996ed49
Emanuel Jose <emanueljose0522@hotmail.com>
2026-05-24 21:56:20 -0400

    compleated the DTOs

---
+++ BudgetTracker/BudgetTracker/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetTracker.DTOs.Auth
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+    }
+}
---
+++ BudgetTracker/BudgetTracker/DTOs/Transactions/TransactionResponseDto.cs
@@ -0,0 +1,28 @@
+﻿using BudgetTracker.Models;
+
+namespace BudgetTracker.DTOs.Transactions
+{
+    public class TransactionResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public decimal SignedAmount { get; set; }
+        public TransactionType Type { get; set; }
+        public Guid CategoryId {  get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public static TransactionResponseDto FromTransaction(Transaction t) => new()
+        {
+            Id              = t.Id,
+            Description     = t.Description,
+            Amount          = t.Amount,
+            SignedAmount    = t.SignedAmount,
+            Type            = t.Type,
+            CategoryId      = t.CategoryId,
+            CreatedAt       = t.CreatedAt,
+            UpdatedAt       = t.UpdatedAt
+        };
+    }
+}
